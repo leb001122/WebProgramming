b2d6cfc1dfda2fbf302b5aea5bbccddfecf7dfea
leb001122 <leb001122@icloud.com>
2022-04-22 21:18:13 +0900

    220422

--- practice/dom/script.js
+++ practice/dom/script.js
@@ -11,9 +11,12 @@ function removeMenu() {
 function menuTemp() {
     let iceMenus = document.getElementsByClassName("ice-menu");
     console.log(iceMenus);
-    for (let i in [...iceMenus]) {
-        iceMenus[i].style.color = "blue"
-    }
+    // for (let i in [...iceMenus]) {
+    //     iceMenus[i].style.color = "blue"
+    // }
+    iceMenus = [...iceMenus];
+    console.log(iceMenus);
+    iceMenus.forEach((x) => (x.style.color = "blue"));
 
     let hotMenus = document.getElementsByClassName("hot-menu");
     console.log(hotMenus);
@@ -61,6 +64,108 @@ function fontSizeUp() {
 }
 
 function myFunction() {
-    var x = document.getElementsByName("fname")[0].tagName;
-    document.getElementsById("demo2").innerHTML = x;
+    var x = document.getElementsByName("fname")[0].value;
+    console.log(x);
+    document.getElementById("demo2").innerText = x;
+}
+
+function getAttributesByProperty() {
+    let div = document.getElementById('myDiv');
+    document.getElementById("demo3").innerText 
+    = div.id + " " + div.className + " " + 
+    div.title + " " + div.lang + " " + div.dir;
+}
+
+function getAttributesByMethod() {
+    let div = document.getElementById('myDiv');
+    document.getElementById("demo3").innerText 
+    = div.getAttribute("id") + " " + div.getAttribute("class") + " " + 
+    div.getAttribute("title") + " " + div.getAttribute('lang') + " " + 
+    div.getAttribute('dir');
+
+    div.removeAttribute('dir');
+}
+
+function getAttributes() {
+    let button = document.getElementById('button');
+    let anchor = document.getElementById('anchor');
+
+    console.log(button.href);
+    console.log(anchor.href);
+    console.log(button.onclick);
+    console.log(anchor.onclick);
+
+}
+
+function setAttributeByProperty() {
+    let button = document.getElementById('button');
+    let anchor = document.getElementById('anchor');
+
+    button.title = 'input Text';
+    button.onclick = "alert('modified click')" // 적용안됨. null이 되어버림.
+    anchor.href = 'https://www.naver.com/';
+    anchor.style.fontSize = '40px'
+}
+
+function setAttributeByMethod() {
+    let button = document.getElementById('button');
+    let anchor = document.getElementById('anchor');
+
+    anchor.setAttribute('href', 'http://cs.kumoh.ac.kr/cs/index.do');
+    anchor.setAttribute('style', 'font-size:30px; border:1px solid blue');
+}
+
+function buttonFunction() {
+    let x = document.getElementsByTagName("H1");
+    x[0].setAttribute("class", "header-text");
+    x[1].setAttribute("class", "body-text")
+}
+
+function setClass() {
+    let classModi = document.getElementById('classModi');
+    classModi.className = 'first';
+    classModi.className = classModi.className + ' second';
+    classModi.className = classModi.className + ' third';
+    console.log(classModi.className);
+}
+
+function setClass2() {
+    let classModi = document.getElementById('classModi');
+    let classes = classModi.className.split(' ');
+    var newClassName = '';
+    for (i of classes) {
+        if (i == 'second') 
+            continue;
+        newClassName += i + ' ';
+    }
+    classModi.className = newClassName;
+}
+
+function addElement() {
+    let root = document.getElementsByTagName('ol')[0]; //root는 살아있는 객체
+    let li = document.createElement('li');
+    let content = document.createTextNode('게임');
+    // li.id = 'newLi';
+    li.className = 'festival';
+    li.appendChild(content);
+    root.appendChild(li); // 요소를 추가할 경우 부모 요소를 먼저 찾아야함.
+
+    li = document.createElement('li');
+    content = document.createTextNode('휴강');
+    li.appendChild(content);
+    root.insertBefore(li, root.firstElementChild.nextElementSibling);  // 원하는 위치에 추가
+}
+
+function removeElement() {  
+    let root = document.getElementsByTagName('ol')[0]; //root는 살아있는 객체
+    let jujum = root.childNodes[2];
+    jujum.remove();
+}
+
+function replaceElement() {  
+    // 삭제+추가
+    let root = document.getElementsByTagName('ol')[0]; //root는 살아있는 객체
+    let li = document.createElement('li');
+    li.textContent = '체육대회';
+    root.replaceChild(li, root.lastElementChild); // 교체할 요소, 교체될 요소
 }
--- project/project2/script.js
+++ project/project2/script.js
@@ -1,7 +1,23 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(pos) {
+    let slides = document.getElementsByClassName("slide");
+
+    if (pos < 1) {
+        slideIndex = slides.length;
+    }
+    else if (pos > slides.length) {
+        slideIndex = 1;
+    }
+
+    let i;
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
 }
-  
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+
+function plusSlides(pos) {
+    showSlides(slideIndex += pos);
 }
