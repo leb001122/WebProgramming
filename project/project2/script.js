a86aaacf478b5d63777804eea28da9d1c0ac19e1
leb001122 <leb001122@icloud.com>
2022-04-23 17:51:25 +0900

    220423

--- project/project2/script.js
+++ project/project2/script.js
@@ -1,3 +1,16 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,4 +33,70 @@ function showSlides(pos) {
 
 function plusSlides(pos) {
     showSlides(slideIndex += pos);
+}
+
+
+
+
+
+
+
+
+let acc = document.getElementsByClassName('accordian');
+var i;
+for (i of [...acc]) {
+    i.addEventListener('click', openPanel);
+}
+function openPanel(event) {
+    this.classList.toggle('active');
+    let panel = this.nextElementSibling;
+    let panels = document.getElementsByClassName('panel');
+
+    if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+    } else {
+        panel.style.display = 'block';
+    }
+    for (i of [...panels]) {
+        if (i === panel) continue;
+        i.style.display = 'none';
+    }
+}
+
+let selectbox = document.getElementById('selectbox');
+selectbox.addEventListener('change', addList);
+let price = 0; 
+
+function addList(event) {
+    let ul = document.getElementById('seleted-list');
+    let selectedValue = selectbox.options[selectbox.selectedIndex].value;
+    var child;
+    for (child of ul.children) {
+        if(child.getAttribute('value') == selectedValue)  
+            return;
+    }
+    let selectedText = selectbox.options[selectbox.selectedIndex].text;
+    let content = document.createTextNode(selectedText);
+    let li = document.createElement('li');
+    li.setAttribute('value', selectedValue);
+    li.appendChild(content);
+    ul.appendChild(li);
+
+    calculate(li)
+    totalAmount();
+}
+
+function totalAmount() {
+    let amount = document.getElementById('amount');
+    amount.textContent = "총 상품금액 " + price + "원";
+
+}
+
+function calculate(li) {
+
+    switch(li.getAttribute('value')) {
+        case 'option-a' : price += 39800; break;
+        case 'option-b' : price += 49800; break;
+        case 'option-c' : price += 59800; break;
+    }
 }
